2a54d801de843bce2828dfe0d8f3924497920e9c
berderskishirang <bederskishirang@gmail.com>
2026-07-20 04:30:42 +0800

    update

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,21 +1,51 @@
+// import { HttpInterceptorFn } from '@angular/common/http';
+
+// export const authInterceptor: HttpInterceptorFn = (req, next) => {
+
+//   const token = localStorage.getItem('token');
+
+//   if(token){
+
+//     req = req.clone({
+
+//       setHeaders:{
+//         Authorization:`Bearer ${token}`
+//       }
+
+//     });
+
+//   }
+
+//   return next(req);
+
+// };
+
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
 
+  const jwtHelper = new JwtHelperService();
+  const router = inject(Router);
+
   const token = localStorage.getItem('token');
 
-  if(token){
+  if (token) {
+
+    if (jwtHelper.isTokenExpired(token)) {
+      localStorage.removeItem('token');
+      router.navigate(['/login']);
+      return next(req);
+    }
 
     req = req.clone({
-
-      setHeaders:{
-        Authorization:`Bearer ${token}`
+      setHeaders: {
+        Authorization: `Bearer ${token}`
       }
-
     });
-
   }
 
   return next(req);
-
 };
